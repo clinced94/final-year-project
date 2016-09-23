4aef0bde350b35ce4dbcf95210e1d63b19b08fd5
David Clince <clinced@tcd.ie>
2016-09-23 15:27:00 +0100

    changed from traffic light colours to all blue

--- app/src/main/java/com/example/clinced/myapplication/HighActivity.java
+++ app/src/main/java/com/example/clinced/myapplication/HighActivity.java
@@ -2,11 +2,17 @@ package com.example.clinced.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by clinced on 17/09/16.
@@ -14,11 +20,20 @@ import android.widget.Toast;
 public class HighActivity extends AppCompatActivity{
 
     ListView listView;
+    public Calendar highCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high);
+
+        highCalendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm");
+        String dateStr = "Recorded on " + dateFormat.format(highCalendar.getTime());
+
+        //TextView textView = (TextView) findViewById(R.id.high_engine_text);
+        //textView.setText(dateStr);
+
 
         //get listview object
         listView = (ListView) findViewById(R.id.high_list);
@@ -28,7 +43,6 @@ public class HighActivity extends AppCompatActivity{
                 "Activity Two", "Activity Three", "Activity Four",
                 "Activity Five", "Activity Six", "Activity Seven",
                 "Activity Eight", "Activity Nine"
-
         };
 
         //Define a new adapter
